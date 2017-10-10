48f55833900d2b29564b06c607f99549d1d9e4bb
logonrm <logonrm@fiap.com.br>
2017-10-09 21:05:37 -0300

    inicial

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RoutingModule } from './routing/routing.module';
+
+import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component' ;
+import { AddComponent } from './add/add.component' ;
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListComponent,
+    AddComponent
+  ],
+  imports: [
+    BrowserModule,
+    RoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
