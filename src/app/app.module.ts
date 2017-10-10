d2e919c956ece103b483ef0bbad76210c14ef296
logonrm <logonrm@fiap.com.br>
2017-10-09 21:11:46 -0300

    inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RoutingModule } from './routing/routing.module';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component' ;
 import { AddComponent } from './add/add.component' ;
@@ -14,7 +16,8 @@ import { AddComponent } from './add/add.component' ;
   ],
   imports: [
     BrowserModule,
-    RoutingModule
+    RoutingModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
